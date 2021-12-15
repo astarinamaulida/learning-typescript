916dbe4cc892fd41390b5f2e9cc3e87dd04ee7fd
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-15 04:19:00 -0500

    Add inferring return types

--- 02_index_Functions.ts
+++ 02_index_Functions.ts
@@ -43,3 +43,20 @@ myproclaim();
 myproclaim("ready?");
 myproclaim("ready!", 3);
 
+
+////////// Inferring Return Types //////////
+
+// TypeScript can also infer the types of values returned by functions. 
+// It does this by looking at the types of the values after a function’s return statements.
+
+function getRandomNumber(){
+  return Math.random();
+}
+
+const myVar = getRandomNumber();
+
+function ouncesToCups(ounces: number) {
+  return `${ounces / 16} cups`;
+}
+ 
+const liquidAmount = ouncesToCups(3);
