5f0c8748c3c4d798a0fc33ea7de023e46c0fc9e9
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-15 04:10:50 -0500

    Add default parameters

--- 02_index_Functions.ts
+++ 02_index_Functions.ts
@@ -12,3 +12,34 @@ function greetTripled(greeting:string, value:number) {
 }
 
 greetTripled('Hiya', 5);
+
+
+////////// Optional Parameters //////////
+
+// To indicate that a parameter is intentionally optional, we add a ? after its name. 
+// This tells TypeScript that the parameter is allowed to be undefined and doesn’t always have to be provided.
+
+function proclaim(status?: string) {
+  console.log(`I'm ${status || 'not ready...'}`);
+}
+
+proclaim();
+proclaim('ready?');
+proclaim('ready!');
+
+
+////////// Default Parameters //////////
+
+// If a parameter is assigned a default value, 
+// TypeScript will infer the variable type to be the same as the default value’s type.
+
+function myproclaim(status = "not ready...", repeat = 1) {
+  for (let i = 0; i < repeat; i += 1) {
+    console.log(`I'm ${status}`);
+  }
+}
+
+myproclaim();
+myproclaim("ready?");
+myproclaim("ready!", 3);
+
