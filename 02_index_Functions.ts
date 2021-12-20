d90fb197fe5c5261ed481cfa0bee997c5d889d69
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-19 23:00:30 -0500

    Add Void return type

--- 02_index_Functions.ts
+++ 02_index_Functions.ts
@@ -74,3 +74,15 @@ function createGreeting(name?: string): string {
   return undefined;
   //Typescript Error: Type 'undefined' is not assignable to type 'string'.
 };
+
+
+////////// Void Return Type //////////
+
+// There is no returned value, so we must treat the return type as void.
+
+function makeFruitSalad(fruit1:string, fruit2:string):void{
+  let salad=fruit1+fruit2+fruit2+fruit1+fruit2+fruit1+fruit1;
+  console.log(salad);
+}
+
+makeFruitSalad('banana','pineapple');
