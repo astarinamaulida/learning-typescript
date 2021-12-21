193b6463dc84d0eeec2f963474b7b5dd9955333f
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-20 22:42:53 -0500

    Add multi dimentional arrays

--- 03_index_Arrays.ts
+++ 03_index_Arrays.ts
@@ -35,3 +35,11 @@ let bestNumbers: number[] = [7,77,4];
 let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
 let bestBreakfasts: string[] = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
 let bestBooleans: boolean[] = [true, false];
+
+
+
+////////// Multi Dimentional Arrays //////////
+
+let bestMealPlan: string[][] = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
+let bestBooleansTwice: boolean[][]= [bestBooleans, bestBooleans];
+let numbersMulti: number[][][] = [ [[1],[2,3]], [[7],bestNumbers] ];
